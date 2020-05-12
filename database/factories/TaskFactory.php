44b9a474b1544ddeaa4cb06a5f6c2c70bffe6781
sunzid02 <sunzid02@gmail.com>
2020-05-12 20:59:50 +0600

    seeder updated

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -4,8 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Task::class, function (Faker $faker) {
     return [
-        'name' => $faker->text( 10),
-        'name' => $faker->boolean(false),
+        'name' => $faker->text(10),
 
     ];
 });
